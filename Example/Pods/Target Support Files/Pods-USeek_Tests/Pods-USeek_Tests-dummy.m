2b03981167ed9f359c75b146386431128101dc1b
chrislin89 <chrislin.317@hotmail.com>
2017-07-25 17:29:59 +0800

    initial commit

---
+++ Example/Pods/Target Support Files/Pods-USeek_Tests/Pods-USeek_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_USeek_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_USeek_Tests
+@end
---
+++ Example/Pods/Target Support Files/USeek/USeek-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_USeek : NSObject
+@end
+@implementation PodsDummy_USeek
+@end
